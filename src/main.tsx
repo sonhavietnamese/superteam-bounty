7947bb9774af9363eefed8aaa27b224fd1d943bf
sonhaaa <nguyensonha.hanz@gmail.com>
2023-01-10 14:12:02 +0700

    init: init project

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Landing from './pages/Landing'
+import React, { FC, useMemo } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { PhantomWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { clusterApiUrl } from '@solana/web3.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+export const Wallet: FC = () => {
+  const network = WalletAdapterNetwork.Devnet
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new UnsafeBurnerWalletAdapter()], [network])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <RouterProvider router={router} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Wallet />)
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//#region STYLES
+const Container = styled.div``
+//#endregion
+
+const Landing = () => {
+  return <div>Landing</div>
+}
+
+export default Landing
